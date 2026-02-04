13c15f00b1422dbae34422599ae7ad485f1ee0f6
Lucia Vega <lucia-soledad.vega@ETUD.UNIV-EIFFEL.FR>
2026-02-04 13:16:58 +0100

    exercise: exchange channels

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <sil/sil.hpp>
 #include <glm/glm.hpp>
+#include <algorithm> 
 
 void keep_green_only(sil::Image& image)
 {
     for (glm::vec3& color : image.pixels())
     {
-        color.r = 0.f; 
-        color.b = 0.f; 
+        color.r = 0.f;
+        color.b = 0.f;
+    }
+}
+
+void swap_red_blue(sil::Image& image)
+{
+    for (glm::vec3& color : image.pixels())
+    {
+        std::swap(color.r, color.b);
     }
 }
 
 int main()
 {
     sil::Image image{"images/logo.png"};
+
     keep_green_only(image); 
-    image.save("output/keep_green_only.png");
+
+    swap_red_blue(image);
+    image.save("output/swap_red_blue.png"); 
+
+    return 0;
 }
